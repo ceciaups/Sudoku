deab346526660030214912acf196258321e27769
ceciaups <cecilia127@msn.com>
2022-11-29 22:05:31 -0500

    Add feature: play and check sudoku

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 - HTML: [/index.html](Sudoku/index.html)
 - CSS: [/css/styles.css](Sudoku/css/styles.css)
 - JS: [/js/script.js](Sudoku/js/script.js)
-![Sudoku page](https://raw.githubusercontent.com/ceciaups/Sudoku/master/Sudoku/images/Sudoku.png)
+
+![Sudoku page](https://raw.githubusercontent.com/ceciaups/Sudoku/master/images/Sudoku.png)
--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ window.onload = function() {
 
   const sudokuBoxSize = 9;
   const sudokuSize = sudokuBoxSize * sudokuBoxSize;
+  const quiz = "004300209005009001070060043006002087190007400050083000600000105003508690042910300";
+  const solution = "864371259325849761971265843436192587198657432257483916689734125713528694542916378";
 
   var sudokuContainer = document.getElementById("sudoku-container");
+  var checkMsg = document.getElementById("check-message");
+  var btnCheck = document.getElementById("button-check");
   
   for (let i=0; i < sudokuBoxSize; i++) {
     let sudokuBox = document.createElement("div");
@@ -12,17 +16,69 @@ window.onload = function() {
     sudokuBox.classList.add("sudoku-box" + i);
 
     for (let j=0; j < sudokuBoxSize; j++) {
+      let number = findNumber(i, j, quiz);
+
       let sudokuInput = document.createElement("input");
-  
-      sudokuInput.classList.add("sudoku-input");
+      
+      if (number == 0) {
+        sudokuInput.classList.add("sudoku-input");
+      }
+      else {
+        sudokuInput.classList.add("sudoku-input");
+        sudokuInput.readOnly = true;
+        sudokuInput.placeholder = number;
+      }
   
       sudokuBox.appendChild(sudokuInput);
     }
-
-    // console.log(i);
-    // console.log(sudokuBox);
     sudokuContainer.appendChild(sudokuBox);
   }
 
-  
+  // Debug mode
+  // for (let i=0; i < sudokuBoxSize; i++) {
+  //   for (let j=0; j < sudokuBoxSize; j++) {
+  //     let sudokuInput = sudokuContainer.childNodes[i].childNodes[j];
+  //     if (sudokuInput.placeholder == 0 && sudokuInput.value == 0) {
+  //       let number = findNumber(i, j, solution);
+  //       sudokuInput.value = number;
+  //     }
+  //   }
+  // }
+
+  function checkSudoku() {
+    let wrongInput = 0;
+    let correctInput = 0;
+    for (let i=0; i < sudokuBoxSize; i++) {
+      for (let j=0; j < sudokuBoxSize; j++) {
+        let sudokuInput = sudokuContainer.childNodes[i].childNodes[j];
+        if (sudokuInput.value != 0) {
+          let number = findNumber(i, j, solution);
+          if (sudokuInput.value != number)
+            wrongInput++;
+          else
+            correctInput++;
+        }
+        else if (sudokuInput.placeholder != 0)
+          correctInput++;
+      }
+    }
+    if (correctInput == sudokuSize) {
+      checkMsg.style.color = "var(--blue-300)";
+      checkMsg.innerHTML = "You have completed the Sudoku!";
+    }
+    else if (wrongInput == 0) {
+      checkMsg.style.color = "var(--blue-300)";
+      checkMsg.innerHTML = "So far so good, keep going!";
+    }
+    else {
+      checkMsg.style.color = "var(--red)";
+      checkMsg.innerHTML = "You have " + wrongInput + " wrong input!";
+    }
+  }
+
+  btnCheck.onclick = checkSudoku;
+}
+
+function findNumber(i, j, array) {
+  return array[Math.floor(j / 3) * 9 + Math.floor(i / 3) * 27 + j % 3 + i % 3 * 3];
 }
