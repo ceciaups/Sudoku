844f3ebfb7b1d42d8e6dfdcd8bb9204d0c1859b6
ceciaups <cecilia127@msn.com>
2022-11-30 01:04:27 -0500

    Add feature: start button

--- js/script.js
+++ js/script.js
@@ -1,49 +1,56 @@
 window.onload = function() {
 
+  // readTextFile("./csv/sudoku.txt");
   const sudokuBoxSize = 9;
   const sudokuSize = sudokuBoxSize * sudokuBoxSize;
   const quiz = "004300209005009001070060043006002087190007400050083000600000105003508690042910300";
   const solution = "864371259325849761971265843436192587198657432257483916689734125713528694542916378";
 
-  var sudokuContainer = document.getElementById("sudoku-container");
+  var startPage = document.getElementById("start-page");
+  var sudokuContainer = document.getElementById("sudoku");
   var checkMsg = document.getElementById("check-message");
+  var btnStart = document.getElementById("button-start");
   var btnCheck = document.getElementById("button-check");
   
-  for (let i=0; i < sudokuBoxSize; i++) {
-    let sudokuBox = document.createElement("div");
+  function genSudoku() {
+    for (let i=0; i < sudokuBoxSize; i++) {
+      let sudokuBox = document.createElement("div");
 
-    sudokuBox.classList.add("sudoku-box");
-    sudokuBox.classList.add("sudoku-box" + i);
+      sudokuBox.classList.add("sudoku-box");
+      sudokuBox.classList.add("sudoku-box" + i);
 
-    for (let j=0; j < sudokuBoxSize; j++) {
-      let number = findNumber(i, j, quiz);
+      for (let j=0; j < sudokuBoxSize; j++) {
+        let number = findNumber(i, j, quiz);
 
-      let sudokuInput = document.createElement("input");
-      
-      if (number == 0) {
-        sudokuInput.classList.add("sudoku-input");
+        let sudokuInput = document.createElement("input");
+        
+        if (number == 0) {
+          sudokuInput.classList.add("sudoku-input");
+        }
+        else {
+          sudokuInput.classList.add("sudoku-input");
+          sudokuInput.readOnly = true;
+          sudokuInput.placeholder = number;
+        }
+    
+        sudokuBox.appendChild(sudokuInput);
       }
-      else {
-        sudokuInput.classList.add("sudoku-input");
-        sudokuInput.readOnly = true;
-        sudokuInput.placeholder = number;
-      }
-  
-      sudokuBox.appendChild(sudokuInput);
+      sudokuContainer.appendChild(sudokuBox);
     }
-    sudokuContainer.appendChild(sudokuBox);
-  }
 
-  // Debug mode
-  // for (let i=0; i < sudokuBoxSize; i++) {
-  //   for (let j=0; j < sudokuBoxSize; j++) {
-  //     let sudokuInput = sudokuContainer.childNodes[i].childNodes[j];
-  //     if (sudokuInput.placeholder == 0 && sudokuInput.value == 0) {
-  //       let number = findNumber(i, j, solution);
-  //       sudokuInput.value = number;
-  //     }
-  //   }
-  // }
+    // Debug mode
+    // for (let i=0; i < sudokuBoxSize; i++) {
+    //   for (let j=0; j < sudokuBoxSize; j++) {
+    //     let sudokuInput = sudokuContainer.childNodes[i].childNodes[j];
+    //     if (sudokuInput.placeholder == 0 && sudokuInput.value == 0) {
+    //       let number = findNumber(i, j, solution);
+    //       sudokuInput.value = number;
+    //     }
+    //   }
+    // }
+    
+    startPage.style.zIndex = -1;
+  }
 
   function checkSudoku() {
     let wrongInput = 0;
@@ -76,9 +83,29 @@ window.onload = function() {
     }
   }
 
+  btnStart.onclick = genSudoku;
   btnCheck.onclick = checkSudoku;
 }
 
 function findNumber(i, j, array) {
   return array[Math.floor(j / 3) * 9 + Math.floor(i / 3) * 27 + j % 3 + i % 3 * 3];
 }
+
+// function readTextFile(file)
+// {
+//     console.log("reading");
+//     var rawFile = new XMLHttpRequest();
+//     rawFile.open("GET", file, false);
+//     rawFile.onreadystatechange = function ()
+//     {
+//         if(rawFile.readyState === 4)
+//         {
+//             if(rawFile.status === 200 || rawFile.status == 0)
+//             {
+//                 var allText = rawFile.responseText;
+//                 alert(allText);
+//             }
+//         }
+//     }
+//     rawFile.send(null);
+// }
