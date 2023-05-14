a4a134e60d420a3d590037d67f4bb42eeed58427
ceciaups <cecilia127@msn.com>
2023-05-14 18:55:39 -0400

    Change array to json

--- js/script.js
+++ js/script.js
@@ -32,6 +32,7 @@ window.onload = function() {
 
     if (response.status === 200) {
       var data = await response.json();
+      console.log(data);
       quiz = data[0];
       solution = data[1];
       callback();
