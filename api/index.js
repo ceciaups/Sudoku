833bdee69362535c3f07eaa5d89a314180077328
ceciaups <cecilia127@msn.com>
2023-05-15 09:46:39 -0400

    Use local path for CSV

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const https = require("https");
 const path = require("path");
 const { parse } = require("csv-parse");
 const app = express();
-var dbUrl = "https://media.githubusercontent.com/media/ceciaups/Sudoku/master/csv/sudoku.csv";
+// var dbUrl = "https://media.githubusercontent.com/media/ceciaups/Sudoku/master/csv/sudoku.csv";
 
 app.use(express.static(__dirname + "/../"));
 
@@ -12,28 +12,40 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/../index.html");
 });
 
-app.get("/sudokuDB/:id", (req, res) => {
-  const dbData = [];
-  const number = Number(req.params.id) + 1;
+app.get("/sudokuDB/:id", async (req, res) => {
+  var dbData = [];
 
-  https.get(dbUrl, (res_db) => {
-    res_db.pipe(parse({ delimiter: ",", from_line: number, to_line: number }))
-    .on("data", (row) => {
-      dbData.push(row);
-    })
-    .on("error", (error) => {
-      console.log(error.message);
-    })
-    .on("end", () => {
-      console.log("Read csv data done!");
+  const nthline = require("nthline"),
+    filePath = __dirname + "/../csv/sudoku.csv",
+    rowIndex = Number(req.params.id);
 
-      var data = {
-        "quiz": dbData[0][0],
-        "solution": dbData[0][1]
-      }
-      res.send(data);
-    })
-  });
+  dbData = (await nthline(rowIndex, filePath)).split(",");
+  
+  var data = {
+    "quiz": dbData[0],
+    "solution": dbData[1]
+  }
+
+  res.send(data);
+
+  // https.get(dbUrl, (res_db) => {
+  //   res_db.pipe(parse({ delimiter: ",", from_line: number, to_line: number }))
+  //   .on("data", (row) => {
+  //     dbData.push(row);
+  //   })
+  //   .on("error", (error) => {
+  //     console.log(error.message);
+  //   })
+  //   .on("end", () => {
+  //     console.log("Read csv data done!");
+
+  //     var data = {
+  //       "quiz": dbData[0][0],
+  //       "solution": dbData[0][1]
+  //     }
+  //     res.send(data);
+  //   })
+  // });
 })
 
 const httpServer = http.createServer(app);
