a2390a656b67f4c3a28eb94f1724d1b888e47011
ceciaups <cecilia127@msn.com>
2023-05-14 23:46:04 -0400

    Modify to read one line in db

--- api/index.js
+++ api/index.js
@@ -5,20 +5,6 @@ const path = require("path");
 const { parse } = require("csv-parse");
 const app = express();
 var dbUrl = "https://media.githubusercontent.com/media/ceciaups/Sudoku/master/csv/sudoku.csv";
-const dbData = [];
-
-https.get(dbUrl, (res) => {
-  res.pipe(parse({ delimiter: ",", from_line: 2 }))
-  .on("data", (row) => {
-    dbData.push(row);
-  })
-  .on("error", (error) => {
-    console.log(error.message);
-  })
-  .on("end", () => {
-    console.log("Read csv data done!");
-  })
-});
 
 app.use(express.static(__dirname + "/../"));
 
@@ -27,9 +13,25 @@ app.get("/", (req, res) => {
 });
 
 app.get("/sudokuDB/:id", (req, res) => {
+  const dbData = [];
+
+  https.get(dbUrl, (res) => {
+    res.pipe(parse({ delimiter: ",", from_line: Number(req.params.id) + 1, to_line: Number(req.params.id) + 1 }))
+    .on("data", (row) => {
+      dbData.push(row);
+    })
+    .on("error", (error) => {
+      console.log(error.message);
+    })
+    .on("end", () => {
+      console.log(dbData);
+      console.log("Read csv data done!");
+    })
+  });
+
   var data = {
-    "quiz": dbData[req.params.id][0],
-    "solution": dbData[req.params.id][1]
+    "quiz": dbData[0][0],
+    "solution": dbData[0][1]
   }
   res.send(data);
 })
