3f779ee98aa2a04e10bedee6e0e727ada774c402
ceciaups <cecilia127@msn.com>
2023-05-15 11:30:02 -0400

    Break down CSV into chunks

--- api/index.js
+++ api/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const http = require("http");
 const https = require("https");
 const path = require("path");
+const fs = require("fs");
 const { parse } = require("csv-parse");
 const app = express();
+const CHUNK_SIZE = 10000000;
+const dbData = [];
 // var dbUrl = "https://media.githubusercontent.com/media/ceciaups/Sudoku/master/csv/sudoku.csv";
 
 app.use(express.static(__dirname + "/../"));
@@ -12,18 +15,41 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/../index.html");
 });
 
+async function readDB() {
+  const stream =  fs.createReadStream(__dirname + "/../csv/sudoku.csv", { highWaterMark: CHUNK_SIZE });
+
+  for await(const db of stream) {
+    let str = (db.toString()).split("\n");
+    console.log(str.length);
+    str.forEach(function(line, index) {
+      if (index == 0 && dbData.length) {
+        let last = dbData.pop();
+        dbData.push(last.concat(line));
+      }
+      else {
+        dbData.push(line);
+      }
+    });
+  }
+}
+
+readDB();
+
 app.get("/sudokuDB/:id", async (req, res) => {
-  var dbData = [];
 
-  const nthline = require("nthline"),
-    filePath = __dirname + "/../csv/sudoku.csv",
-    rowIndex = Number(req.params.id);
+  // var dbData = [];
 
-  dbData = (await nthline(rowIndex, filePath)).split(",");
+  // const nthline = require("nthline"),
+  //   filePath = __dirname + "/../csv/sudoku.csv",
+  //   rowIndex = Number(req.params.id);
+
+  // dbData = (await nthline(rowIndex, filePath)).split(",");
   
-  var data = {
-    "quiz": dbData[0],
-    "solution": dbData[1]
+  let split = dbData[req.params.id].split(",");
+
+  let data = {
+    "quiz": split[0],
+    "solution": split[1]
   }
 
   res.send(data);
