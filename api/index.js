8816a8c5e59719a0439a898a88cba8559b4b60e1
ceciaups <cecilia127@msn.com>
2023-05-16 10:38:52 -0400

    Get data from Vercel database

--- api/index.js
+++ api/index.js
@@ -4,25 +4,28 @@ const https = require("https");
 const path = require("path");
 const app = express();
 
+const { sql } =  require('@vercel/postgres');
+const dotenv = require("dotenv");
+dotenv.config();
+
 app.use(express.static(__dirname + "/../"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/../index.html");
 });
 
-app.get("/sudokuDB/:id", (req, res) => {
+app.get("/sudokuDB/:id", async (req, res) => {
 
-  // const client = await db.connect();
-  // const query = await client.sql`SELECT * FROM Sudokus WHERE id = ${req.params.id};`;
-  // const sudoku = query.rows[0];
-  // return res.status(200).send(sudoku);
+  const { rows } = await sql`SELECT * FROM Sudokus WHERE id = ${req.params.id};`;
+  const sudoku = rows[0];
+  return res.status(200).send(sudoku);
 
-  var data = {
-    "quiz": "004300209005009001070060043006002087190007400050083000600000105003508690042910300",
-    "solution": "864371259325849761971265843436192587198657432257483916689734125713528694542916378"
-  }
+  // var data = {
+  //   "quiz": "004300209005009001070060043006002087190007400050083000600000105003508690042910300",
+  //   "solution": "864371259325849761971265843436192587198657432257483916689734125713528694542916378"
+  // }
 
-  res.send(data);
+  // res.send(data);
 })
 
 const httpServer = http.createServer(app);
