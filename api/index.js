2ef194e97a7c31c0f1f8101e0cab6636812b0606
ceciaups <cecilia127@msn.com>
2023-05-16 10:46:22 -0400

    Finalize the project

--- api/index.js
+++ api/index.js
@@ -15,17 +15,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/sudokuDB/:id", async (req, res) => {
-
   const { rows } = await sql`SELECT * FROM Sudokus WHERE id = ${req.params.id};`;
   const sudoku = rows[0];
   return res.status(200).send(sudoku);
-
-  // var data = {
-  //   "quiz": "004300209005009001070060043006002087190007400050083000600000105003508690042910300",
-  //   "solution": "864371259325849761971265843436192587198657432257483916689734125713528694542916378"
-  // }
-
-  // res.send(data);
 })
 
 const httpServer = http.createServer(app);
