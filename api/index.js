a357e3d2cdc715a0ce6338fff151989a169fd8a7
ceciaups <cecilia127@msn.com>
2023-05-16 10:33:27 -0400

    Hardcode data

--- api/index.js
+++ api/index.js
@@ -3,10 +3,6 @@ const http = require("http");
 const https = require("https");
 const path = require("path");
 const app = express();
-const { db } =  require('@vercel/postgres');
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 app.use(express.static(__dirname + "/../"));
 
@@ -14,11 +10,19 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/../index.html");
 });
 
-app.get("/sudokuDB/:id", async (req, res) => {
-  const client = await db.connect();
-  const query = await client.sql`SELECT * FROM Sudokus WHERE id = ${req.params.id};`;
-  const sudoku = query.rows[0];
-  return res.status(200).send(sudoku);
+app.get("/sudokuDB/:id", (req, res) => {
+
+  // const client = await db.connect();
+  // const query = await client.sql`SELECT * FROM Sudokus WHERE id = ${req.params.id};`;
+  // const sudoku = query.rows[0];
+  // return res.status(200).send(sudoku);
+
+  var data = {
+    "quiz": "004300209005009001070060043006002087190007400050083000600000105003508690042910300",
+    "solution": "864371259325849761971265843436192587198657432257483916689734125713528694542916378"
+  }
+
+  res.send(data);
 })
 
 const httpServer = http.createServer(app);
@@ -28,3 +32,4 @@ httpServer.listen(80, function() {
 })
 
 module.exports = app;
+  
